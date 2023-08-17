5f05bc13cb4d73f165fafff6a8054290748a9be2
irenegerrish <142243601+irenegerrish@users.noreply.github.com>
2023-08-17 09:53:33 -0500

    Day 4

--- Day02/Lecture/# Homework 1: due August 18, 2023.py
+++ Day02/Lecture/# Homework 1: due August 18, 2023.py
@@ -7,6 +7,8 @@ class Client:
         self.cash_balance = cash_balance
         self.mutual_funds = mutual_funds
         self.stocks = stocks
+        portfolio = self.cash_balance + self.mutual_funds + self.stocks
+
 
     def calculate_portfolio_value(self):
         return self.cash_balance + self.mutual_funds + self.stocks
@@ -23,4 +25,29 @@ class Portfolio:
     def __init__(self, client_id, cash_balance=0, mutual_funds=0, stocks=0):
         self.client = Client(client_id, cash_balance, mutual_funds, stocks)
 
-Portfolio()
+
+
+# Example usage
+client_id = 1
+portfolio = clients.get(client_id)
+if portfolio:
+    print("Original Portfolio:")
+    print("Cash Balance:", portfolio.client.cash_balance)
+    print("Mutual Funds:", portfolio.client.mutual_funds)
+    print("Stocks:", portfolio.client.stocks)
+
+    # Buy $1000 worth of mutual funds
+    portfolio.client.buy_mutual_funds(1000)
+    print("Portfolio after buying mutual funds:")
+    print("Cash Balance:", portfolio.client.cash_balance)
+    print("Mutual Funds:", portfolio.client.mutual_funds)
+    print("Stocks:", portfolio.client.stocks)
+
+    # Sell $2000 worth of stocks
+    portfolio.client.sell_stocks(2000)
+    print("Portfolio after selling stocks:")
+    print("Cash Balance:", portfolio.client.cash_balance)
+    print("Mutual Funds:", portfolio.client.mutual_funds)
+    print("Stocks:", portfolio.client.stocks)
+else:
+    print("Client not found.")
